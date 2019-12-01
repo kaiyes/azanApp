c86277930aded08950d79f26802886422ed45b2e
kaiyes <kaiyes.ansary@gmail.com>
2019-12-01 19:31:30 +0600

    changed name

--- android/app/src/main/java/com/salat/MainApplication.java
+++ android/app/src/main/java/com/salat/MainApplication.java
@@ -1,4 +1,4 @@
-package com.adminapp;
+package com.salat;
 
 import android.app.Application;
 import android.content.Context;
--- ios/SalatTests/SalatTests.m
+++ ios/SalatTests/SalatTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AdminAppTests : XCTestCase
+@interface SalatTests : XCTestCase
 
 @end
 
-@implementation AdminAppTests
+@implementation SalatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
